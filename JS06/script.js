c5701c6a358b9c9bbe74668afb0a623852785c70
George Artenyan <george.artenyan@gmail.com>
2024-12-11 19:53:03 +0400

    js06

---
+++ JS06/script.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+// const button = document.getElementById("btn");
+
+// button.removeAttribute("class");
+// button.setAttribute("class", "green");
+
+// button.innerText = "Green Button";
+// button.style.border = "4px solid purple";
+
+// button.addEventListener("click", () => {
+//   alert("Button clicked!");
+// });
+
+// button.addEventListener("mouseover", () => {
+//   button.style.backgroundColor = "lightgreen";
+// });
+
+// button.addEventListener("mouseout", () => {
+//   button.style.backgroundColor = "white";
+// });
+
+// const onesList = document.getElementsByClassName("one");
+// for (let i = 0; i < onesList.length; i++) {
+//   onesList[i].style.color = "red";
+// }
+
+// const allPTags = document.getElementsByTagName("p");
+// for (let i = 0; i < allPTags.length; i++) {
+//   if (allPTags[i].innerText === "Footer") {
+//     allPTags[i].style.color = "blue";
+//   }
+// }
+
+// const someDiv = document.getElementById("some");
+
+// someDiv.innerHTML = "<p>Some text</p>";
